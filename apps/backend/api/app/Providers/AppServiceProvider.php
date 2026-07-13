983e1a73be390d1a0f9269d8d136af85d02f1f12
Kareem Kadry <cr7.rm35@gmail.com>
2026-07-13 14:03:34 +0300

    solve conflicts

--- apps/backend/api/app/Providers/AppServiceProvider.php
+++ apps/backend/api/app/Providers/AppServiceProvider.php
@@ -6,16 +6,13 @@ use App\Repositories\Eloquent\AdminRepository;
 use App\Repositories\Eloquent\UserRepository;
 use App\Repositories\Interfaces\UserRepositoryInterface;
 use App\Repositories\Eloquent\RestaurantCategoryRepository;
-<<<<<<< HEAD
 use App\Repositories\Eloquent\PhoneVerificationRepository;
 use App\Repositories\Interfaces\PhoneVerificationRepositoryInterface;
-=======
 use App\Repositories\Eloquent\RestaurantRepository;
 use App\Repositories\Interfaces\AdminRepositoryInterface;
 use App\Repositories\Interfaces\RestaurantCategoryRepositoryInterface;
 use App\Repositories\Interfaces\RestaurantRepositoryInterface;
 use Illuminate\Support\ServiceProvider;
->>>>>>> origin/master
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -37,13 +34,13 @@ class AppServiceProvider extends ServiceProvider
         );
 
         $this->app->bind(
-<<<<<<< HEAD
-            PhoneVerificationRepositoryInterface::class,
-            PhoneVerificationRepository::class
-=======
             RestaurantRepositoryInterface::class,
             RestaurantRepository::class
->>>>>>> origin/master
+        );
+
+        $this->app->bind(
+            PhoneVerificationRepositoryInterface::class,
+            PhoneVerificationRepository::class
         );
     }
 
--- apps/backend/api/routes/api.php
+++ apps/backend/api/routes/api.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\Api\Auth\UserAuthController;
 use App\Http\Controllers\Api\RestaurantCategoryController;
 use App\Http\Controllers\Api\Auth\OtpController;
 
-<<<<<<< HEAD
 
 Route::prefix('admin')->name('admin.')->group(function () {
 
@@ -27,8 +26,7 @@ Route::prefix('admin')->name('admin.')->group(function () {
     });
 });
 
-=======
->>>>>>> origin/master
+
 //user auth
 Route::prefix('user')->name('user.')->group(function () {
     Route::post('/send-otp', [OtpController::class, 'sendOtp']);
